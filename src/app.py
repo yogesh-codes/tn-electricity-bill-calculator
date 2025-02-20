02d47cc00c368f86abf17bf6911bf0bc1873cc8d
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-02-20 08:42:52 +0530

    ok- both calculate_billAmount and determine_unitsConsumption are working

--- src/app.py
+++ src/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 # API route to calculate bill
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
+@app.route('/calculate_billAmount', methods=['POST'])
+def calculate_billAmount():
     data = request.json
     try:
 
@@ -29,6 +29,46 @@ def calculate():
 
     except ValueError as e:
         return jsonify({'error': str(e)}), 400
+
+@app.route('/page1', methods=['GET','POST'])
+def page1():
+    try:
+        return render_template("page1.html")
+
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+@app.route('/page2', methods=['GET','POST'])
+def page2():
+    try:
+        return render_template("page2.html")
+
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+
+# API to determine units using price:
+
+@app.route('/determine_unitsConsumed', methods=['POST'])
+def determine_unitsConsumed_by_billAmount():
+    data = request.json
+    try:
+
+        billAmount = int(data.get('billAmount', 0))
+        
+        validate.isPositiveInt(billAmount)
+        
+        result=t.determine_units_using_billAmount(billAmount)
+        unitsConsumed=result
+        
+        print(f"return value is {unitsConsumed} units.")
+
+        return jsonify({'unitsConsumed': unitsConsumed, 'billAmount': billAmount})
+
+    except ValueError as e:
+        return jsonify({'error': str(e)}), 400
+
+
 
 # Home route (optional)s
 @app.route('/')
--- src/tn_funcs.py
+++ src/tn_funcs.py
@@ -18,7 +18,7 @@ df_over500=df_over500.set_index('slab')
 df_over500
 
 
-def compute_slabwise_consumption(*,units_consumed:int,df:pd.DataFrame):
+def __compute_slabwise_consumption(*,units_consumed:int,df:pd.DataFrame):
     slabmarkers=df["start"].tolist()
     slabmarkers.append(np.inf)
 
@@ -43,7 +43,7 @@ def compute_slabwise_consumption(*,units_consumed:int,df:pd.DataFrame):
     
     df["myunits"]=L
     
-def compute_slabwise_price(*,df:pd.DataFrame):
+def __compute_slabwise_price(*,df:pd.DataFrame):
      df["myunits_price"]=df["rate"]*df["myunits"]
 
 def calculate_bill(units_consumed):
@@ -56,8 +56,8 @@ def calculate_bill(units_consumed):
 
     
 
-    compute_slabwise_consumption(units_consumed=units_consumed,df=df)
-    compute_slabwise_price(df=df)
+    __compute_slabwise_consumption(units_consumed=units_consumed,df=df)
+    __compute_slabwise_price(df=df)
 
 
     return {'amount':int(df["myunits_price"].sum()),'df':df,"units_consumed":units_consumed}
@@ -76,3 +76,69 @@ import seaborn as sns
 
 # given a price, determine units consumed
 
+## Reverse lookup---------------------------------------------------
+
+#CONFIG
+filename=os.path.join(DATA_FOLDER,r"processed/precomputed_ltable_unitsconsumed_price.csv")
+
+#Auto
+def __load_reverse_ltable(filename):
+
+
+    df=pd.read_csv(filepath_or_buffer=filename)
+    data_inverted = df.to_dict(orient="list") 
+    data_inverted= dict(zip(df["value"],df["key"]))
+    return data_inverted
+
+
+data_inverted=__load_reverse_ltable(filename)
+
+
+def determine_units_using_billAmount(price):
+    def __determine_border_prices(L:list,x:int): 
+        '''almost like binary search'''
+        low=0
+        high=len(L)-1
+
+        i=0
+        while (low<high and i<200):
+            print(f"boundary {low} to {high}")
+            i+=1
+
+            mid=int((low+high)/2)
+            if x==L[mid]:
+                break
+            elif x>L[mid]:
+                
+                low=mid
+            
+            else:
+                high=mid
+
+            if low+1==high:
+                break
+               
+        print(f"boundary {low} to {high}")
+        low=low
+        high=high#int(np.round(high))
+        print (L[low],L[high])
+        return (L[low],L[high])
+
+    def __determine_corresponding_border_units(lowerindex,upperindex,data_inverted):
+        lowerprice=data_inverted[lowerindex]
+        upperprice=data_inverted[upperindex]
+
+        return lowerprice,upperprice
+
+    def __linear_interpolation(x,z1,z2):
+        x1,y1=z1
+        x2,y2=z2
+        return y1 + (x - x1) / (x2 - x1) * (y2 - y1)
+
+ 
+    lowerprice,upperprice=__determine_border_prices(list(data_inverted.keys()),price)
+    lowerunits,upperunits=__determine_corresponding_border_units(lowerprice,upperprice,data_inverted)
+
+    esti_units=__linear_interpolation(price,(lowerprice,lowerunits),(upperprice,upperunits))
+    
+    return int(esti_units)
