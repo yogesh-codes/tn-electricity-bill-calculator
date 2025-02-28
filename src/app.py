6cbfb8b5410e88aca3cb06f4dccc8056ae788c37
Yogesh <139562574+yogesh-codes@users.noreply.github.com>
2025-02-28 17:40:57 +0530

    commit before trying out server

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, jsonify, render_template, Blueprint
-import tn_funcs as t # found in same folder
-from utils import validate # found in same folder
+from flask import Flask
 from waitress import serve
 
 from routes.api_routes import api_routes
@@ -14,6 +12,6 @@ app.register_blueprint(page_routes)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    #serve(app,host="0.0.0.0",port=5000)
-    print("Finished running")
+    #app.run(debug=True)
+    serve(app,host="0.0.0.0",port=5000)
+    print("Finished running. App closing.")
